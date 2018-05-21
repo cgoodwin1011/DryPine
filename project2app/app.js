6944a38c8dd1eb79e41460075559cd850c68f03a
Karina Khachatryan <karinakhachat@Karinas-Air.fios-router.home>
2018-05-20 23:05:52 -0400

    update passport.js

--- project2app/app.js
+++ project2app/app.js
@@ -1,48 +1,34 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const bcrypt = require("bcrypt")
+var express = require("express");
+var bodyParser = require("body-parser");
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
+var path = require("path")
 
+// Setting up port and requiring models for syncing
+var PORT = process.env.PORT || 8080;
+var db = require("./models");
+
+// Creating express app and configuring middleware needed for authentication
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 require("./routes/index.js")(app);
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(8080, () => console.log('Example app listening on port 3000!'))
 
-const passport = require('passport')
-const bcrypt = require('bcrypt')
-const LocalStrategy = require('passport-local').Strategy
 
-const user = {
-  username: 'test-user',
-  passwordHash: 'bcrypt-hashed-password',
-  id: 1
-}
-
-passport.use(new LocalStrategy(
- (username, password, done) => {
-    findUser(username, (err, user) => {
-      if (err) {
-        return done(err)
-      }
-
-      // User not found
-      if (!user) {
-        return done(null, false)
-      }
-
-      // Always use hashed passwords and fixed time comparison
-      bcrypt.compare(password, user.passwordHash, (err, isValid) => {
-        if (err) {
-          return done(err)
-        }
-        if (!isValid) {
-          return done(null, false)
-        }
-        return done(null, user)
-      })
-    })
-  }
-))
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+  });
+});
