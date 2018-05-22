6a0b017b029454c640105a4d0a6d6e52d1b74d78
Charles Goodwin <cgoodwin2127@gmail.com>
2018-05-22 17:28:10 -0400

    updating package-lock.json

--- server.js
+++ server.js
@@ -1,60 +1,66 @@
 // dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var session = require("express-session");
 var exphbs = require('express-handlebars');
 var mysql = require('mysql');
 // var methodOverride = require('method-override');
-// var routes = require("./controllers/api-routes.js");
+
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
+var path = require("path");
 
 // set up expree app
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 // set up express with bodyParser
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 // t-up server directory 
 app.use(express.static("public"));
 
-// var routes = require('./controllers/beer_controllers.js');
-
-// set up handlebars -- need to revise since using Pug/Jade
-// app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-// app.set('view engine', 'handlebars');
-
-// app.use('/', routes);
-// app.use('/api/allposts', routes);
-// app.use('/api/publicposts', routes);
-// app.use('/api/userposts/:Id', routes);
-// app.use('/api/userposts/:Id', routes);
-// app.use('/api/userConversations/:Id', routes);
-
 // Requiring our models for syncing
 var db = require("./models");
 
-// Set up the Express app to handle data parsing
-
-// parse application/x-www-form-urlencoded
+// set up body parser for use with express
 app.use(bodyParser.urlencoded({ extended: true }));
-// parse application/json
 app.use(bodyParser.json());
 
 // Static directory
 app.use(express.static("public"));
 
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+
 // Routes
 // =============================================================
 require("./controllers/api-routes.js")(app);
-// require("./controlers/html-routes.js")(app);
+
+
+
+// start server
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-// db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
+});
+
+// db.sequelize.sync({ force: true }).then(function() {
+  // app.listen(PORT, function() {
+  //   console.log("App listening on PORT " + PORT);
+  // });
 // });
