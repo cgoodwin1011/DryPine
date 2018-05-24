a12d4898e9e65fabbe74c0a00d59f8b22d461beb
Charles Goodwin <cgoodwin2127@gmail.com>
2018-05-24 17:29:38 -0400

    cleaned up files

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -7,12 +7,11 @@ var db = require("../models");
 // the routes
 
 module.exports = function (app) {
-
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
-  //get all posts
+  // get all posts in no specified order (presumably by ID)
   // returning them as JSON.
   app.get("/api/allposts", function (req, res) {
     console.log("hi");
@@ -23,6 +22,8 @@ module.exports = function (app) {
       });
   });
 
+
+  // returns posts sorted into threads
   app.get("/api/postsInOrder", function(req, res) {
     console.log("calling posts in order");
     db.Post.findAll({
@@ -34,6 +35,8 @@ module.exports = function (app) {
 
   });
 
+
+  // returns any partical post by postId
   app.get("/api/thisPost/:id", function (req, res) {
     db.Post.findAll({
       where: {
@@ -44,7 +47,7 @@ module.exports = function (app) {
     });
   });
 
-  //get all posts by user
+  // returns all posts by a user
   app.get("/api/userPosts/:Id", function (req, res) {
     var theUser = req.params.Id;
     db.Post.findAll({
@@ -56,6 +59,7 @@ module.exports = function (app) {
     });
   });
 
+  // returns thread starting posts
   app.get("/api/originalPosts", function (req, res) {
     db.Post.findAll( {
       where: {
@@ -69,49 +73,36 @@ module.exports = function (app) {
     });
   });
 
-  // Add a thread
-  app.post("/api/newthread", function (req, res) {
-    // var selfReply;
-    // db.Post.max('postId').then(
-    //   function (result) {
-    //     selfReply = result+1;
+  // starts a new thread
+app.post("/api/newthread", function (req, res) {
       db.Post.create({
         author: req.body.author,
         replyingTo: -1,
         pTimestamp: req.body.created_at,
         content: req.body.content,
       }).then(function (result) {
-        // console.log(result.get('postId'))
-        var temp
+        // return to the db to get the post's ID 
+        // SQL db won't supply ID until after the post is created.  
+        // for initial posts, the replyingTo: field is set to postID....
+        var temp;
         db.Post.findAll({
           where: {replyingTo: -1}
-        }).then(function(result) {
+          }).then(function(result) {
           temp = result[0].postId;
-          db.Post.update({replyingTo: temp}, {
-            where: {postId: temp}
-          },
-        ).then(function(nextresult) {console.log("nothing")});
+          db.Post.update({replyingTo: temp}, 
+            {
+              where: {postId: temp}
+            }).then(function(nextresult) {console.log("nothing");});
         });
         res.end();
-      })
-
-      // db.Post.findAll({
-      //   replyingTo: { $col: 'db.Post.postId'}
-      //   }, {
-      //   where: {replyingTo: -1}
-      // }
-      // ).then(function(result) {
-      //   console.log("all with missing replyTo's")
-      //   console.log(result);
-      // });
+      });
   });
 
-
-
+  // adds a reply to an existing thread.  
+  // replyingto field is set to ID of first post in thread.  
   app.post("/api/newreply/:id", function (req, res) {
     db.Post.create({
       author: req.body.author,
-      // replyingTo: 99,
       replyingTo: parseInt(req.params.id),
       pTimestamp: req.body.created_at,
       content: req.body.content,
@@ -119,14 +110,4 @@ module.exports = function (app) {
       res.redirect('/');
     });
   });
-
-  // app.get("api/replypost/:id", function (req, res) {
-  // });
-
-  // should be an HTML route
-  // app.get("/api/newthread", function (req, res) {
-  //   // res.sendFile(path.join(__dirname, "../newPostTest.html"));
-  //   // path.join(__dirname, "../newPostTest.html")
-  // });
-
 };
